cbd4038388f7cef1670c38da70a35cba64c39b06
Matias <matiaspupirivarola@gmail.com>
2025-06-25 16:49:39 -0300

    agregando un boton

--- bebidas.js
+++ bebidas.js
@@ -1,7 +1,22 @@
 const carrito = [];
-const lista = document.querySelectorAll('#Bebidas-cal input[type="checkbox"]');
+const lista1 = document.querySelectorAll('#Bebidas-cal input[type="checkbox"]');
+const lista2 = document.querySelectorAll('#Bebidas-fri input[type="checkbox"]');
+lista1.forEach((checkbox) => {
+  checkbox.addEventListener("change", () => {
+    const texto = checkbox.parentElement.textContent.trim();
 
-lista.forEach((checkbox) => {
+    if (checkbox.checked) {
+      // si esta marcado
+      carrito.push(texto); // se introduce en la array
+    } else {
+      const i = carrito.indexOf(texto); // busca la posicion de texto en el array
+      if (i !== -1) carrito.splice(i, 1); // si lo encuentra lo elimina
+    }
+    agregarCarrito();
+  });
+});
+
+lista2.forEach((checkbox) => {
   checkbox.addEventListener("change", () => {
     const texto = checkbox.parentElement.textContent.trim();
 
@@ -20,7 +35,18 @@ function agregarCarrito() {
   const agrega = document.getElementById("carrito");
   let contenido = "";
   for (let i = 0; i < carrito.length; i++) {
-    contenido += `<li class="list-group-item">${carrito[i]}</li>`;
+    contenido += `<li class="list-group-item d-flex align-items-center gap-2">${carrito[i]}</li>`;
   }
   agrega.innerHTML = contenido;
 }
+
+let botonPedido = document.createElement("button");
+botonPedido.setAttribute("class", "btn-color mt-3 mx-auto d-grid");
+botonPedido.textContent = "Confirmar";
+
+botonPedido.addEventListener('click', () {
+  console.log("se preciono el boton")
+})
+
+
+carrito_compras.appendChild(botonPedido);
